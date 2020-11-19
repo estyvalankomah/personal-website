11b26b651417db309d56e800341da5a3a72f14c1
unknown <abenammra2002@gmail.com>
2020-11-19 22:20:59 +0000

    still tailoring it to suit my portfolio

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -17,40 +17,34 @@ class Header extends Component {
 
     return (
       <header id="home">
-
-      <nav id="nav-wrap">
-
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
-      </nav>
-
-      <div className="row banner">
-         <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a <span>{city}</span> based <span>{occupation}</span>, 
-            {description}<span>{employer}</span> where I work as a 
-            <span> {jobDescription}</span>.</h3>
-            <hr />
-            <ul className="social">
-               {networks}
+         <nav id="nav-wrap">
+            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+            <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+            <ul id="nav" className="nav">
+               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+               <li><a className="smoothscroll" href="#about">About</a></li>
+               <li><a className="smoothscroll" href="#resume">Resume</a></li>
+               <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+               <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+               <li><a className="smoothscroll" href="#contact">Contact</a></li>
             </ul>
+         </nav>
+         <div className="row banner">
+            <div className="banner-text">
+               <h1 className="responsive-headline">I'm {name}.</h1>
+               <h3>I am an <span>{city}</span> based <span>{occupation}</span>, 
+               {description}<span>{employer}</span> where I work as a 
+               <span> {jobDescription}</span>.</h3>
+               <hr />
+               <ul className="social">
+                  {networks}
+               </ul>
+            </div>
          </div>
-      </div>
-
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
-
-   </header>
+         <p className="scrolldown">
+            <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+         </p>
+      </header>
     );
   }
 }
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -28,49 +28,38 @@ class Resume extends Component {
 
     return (
       <section id="resume">
-
-      <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-            <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
-            </div>
-         </div>
-      </div>
-
-
-      <div className="row work">
-
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          {work}
+        <div className="row education">
+          <div className="three columns header-col">
+              <h1><span>Education</span></h1>
+          </div>
+          <div className="nine columns main-col">
+              <div className="row item">
+                <div className="twelve columns">
+                  {education}
+                </div>
+              </div>
+          </div>
         </div>
-    </div>
-
-
-
-      <div className="row skill">
-
-         <div className="three columns header-col">
-            <h1><span>Favorite Tech</span></h1>
-         </div>
-
-         <div>
-           <div className="nine columns main-col"><p className="lead center">{skillmessage}</p></div>
-				   <ul className="bgrid-quarters s-bgrid-thirds cf">
-					  {skills}
-					 </ul>
-			  </div>
-      </div>
-   </section>
+        <div className="row work">
+          <div className="three columns header-col">
+              <h1><span>Work</span></h1>
+          </div>
+          <div className="nine columns main-col">
+            {work}
+          </div>
+        </div>
+        <div className="row skill">
+          <div className="three columns header-col">
+              <h1><span>Favorite Tech</span></h1>
+          </div>
+          <div>
+            <div className="nine columns main-col"><p className="lead center">{skillmessage}</p></div>
+            <ul className="bgrid-quarters s-bgrid-thirds cf">
+              {skills}
+            </ul>
+          </div>
+        </div>
+      </section>
     );
   }
 }
