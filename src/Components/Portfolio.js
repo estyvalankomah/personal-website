ae9d385169205fd12e596fcd6135f185f76745b0
unknown <abenammra2002@gmail.com>
2020-12-16 09:37:54 +0000

    save me

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -7,7 +7,7 @@ class Portfolio extends Component {
       var projects = this.props.data.projects.map(function(projects){
         var projectImage = 'images/portfolio/'+projects.image;
         return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
+          <div className="item-wrap">
             <a href={projects.url} title={projects.title}>
                <img alt={projects.title} src={projectImage} />
                <div className="overlay">
@@ -16,7 +16,7 @@ class Portfolio extends Component {
                      <p>{projects.category}</p>
                   </div>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
+                <div className="link-icon"><i className="fa fa-link"></i></div>
             </a>
           </div>
         </div>
